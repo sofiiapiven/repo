0b8d622c224057514533c749a13b2b8593679ed7
sofiiapiven <135251880+sofiiapiven@users.noreply.github.com>
2023-06-04 18:43:58 +0200

    Update prio_model.py

--- prio_model.py
+++ prio_model.py
@@ -52,9 +52,11 @@ with st.expander("Select Tags"):
             if st.checkbox(tag):
                 selected_tags.append(tag)
             
+df2['Review_Date'] = pd.to_datetime(df2['Review_Date'])
+        
+df2.columns = df2.columns.str.strip()
 
 # Apply the filter to the DataFrame
-df2['Review_Date'] = pd.to_datetime(df2['Review_Date'])
 selected_date = st.date_input('Select Date', [df2['Review_Date'].min().date(), df2['Review_Date'].max().date()], key='date_range')
 
 rank_min = st.slider('Minimum Reviewer Ranking Score', min_value=-0.113486, max_value=0.333398, value=-0.113486)
